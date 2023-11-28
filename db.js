a07b56e7ceaf714ecd014604c94274dec3198b33
Emi-re997 <emilianore997@gmail.com>
2023-11-28 12:32:30 -0300

    Preparando para el deploy

--- db.js
+++ db.js
@@ -15,6 +15,24 @@ async function testDatabaseConnection() {
   }
 }
 
-testDatabaseConnection();
+// Sincronizar la base de datos
+async function syncDatabase() {
+    try {
+      // Sincroniza el modelo con la base de datos
+      await sequelize.sync({ alter: true });
+      console.log('Base de datos sincronizada correctamente.');
+    } catch (error) {
+      console.error('Error al sincronizar la base de datos:', error.message);
+      throw error;
+    }
+  }
+  
+  // Ejecutar la sincronización y conexión al iniciar la aplicación
+  async function initializeDatabase() {
+    await testDatabaseConnection();
+    await syncDatabase();
+  }
+  
+  initializeDatabase();
 
 module.exports = sequelize;
