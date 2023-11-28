940ced6c5e02b449a9921a91f3934bd9333a775a
Emi-re997 <emilianore997@gmail.com>
2023-11-28 12:35:40 -0300

    Arreglando errores en rutas

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
-
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
